35115443fb0eb339fab2513ff9bc04caea29aba8
Ben Chaimo <doroza@hotmail.com>
2019-06-10 02:12:54 +0300

    serverside file upload overhaul

--- RipOffAPI/Controllers/FileUploadController.cs
+++ RipOffAPI/Controllers/FileUploadController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
@@ -16,9 +17,12 @@ namespace RipOffAPI.Controllers
         [HttpPost]
         public async Task<IHttpActionResult> UploadFile()
         {
+            var identity = (ClaimsIdentity)User.Identity;
+            int iuid = Convert.ToInt32(identity.FindFirst("user_id").Value);
             var ctx = HttpContext.Current;
-            var root = ctx.Server.MapPath("~/App_Data/Images/Users");
+            var root = ctx.Server.MapPath("~/App_Data/Images/Users/");
             var provider = new MultipartFormDataStreamProvider(root);
+
 
             try
             {
@@ -26,12 +30,19 @@ namespace RipOffAPI.Controllers
 
                 foreach (var file in provider.FileData)
                 {
-                    var name = file.Headers.ContentDisposition.FileName;
+                    var lastDot = file.Headers.ContentDisposition.FileName.LastIndexOf('.');
+                    var extension = file.Headers.ContentDisposition.FileName.Substring(lastDot); 
+                    var name = iuid.ToString() + extension;
 
                     name = name.Trim('"');
 
                     var localFileName = file.LocalFileName;
                     var filePath = Path.Combine(root, name);
+
+                    if (File.Exists(filePath))
+                    {
+                        File.Delete(filePath);
+                    }
 
                     File.Move(localFileName, filePath);
                 }
